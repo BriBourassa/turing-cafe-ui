1b22339377f984b4fce0f41ce450a8295485b42d
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-05-30 12:11:27 -0600

    Add error handling in ResList in form of early return

--- src/Components/ResList/ResList.js
+++ src/Components/ResList/ResList.js
@@ -3,7 +3,7 @@ import './ResList.css'
 import SingleRes from '../SingleRes/SingleRes';
 
 const ResList = ({ allRes, deleteRes }) => {
-// console.log(allRes, 'line 7')
+
     const mappedRes = allRes.map((res) => {
         return ( 
             <SingleRes 
@@ -12,13 +12,16 @@ const ResList = ({ allRes, deleteRes }) => {
                 deleteRes={deleteRes}/>
                 )
             })
-            // console.log(mappedRes, 'mapped!')
 
+    if(!mappedRes.length) {
+        return <p className="oops">Oops, my bad</p>
+    }
+            
 return(
     <section className="res-container">
         {mappedRes}
     </section>
     )
-}
+};
 
 export default ResList
